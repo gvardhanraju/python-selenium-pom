7f0fb42c359faaca3e1a20bbf188baea5dcdf7b1
gvardhanraju <155356650+gvardhanraju@users.noreply.github.com>
2025-07-07 17:41:00 +0530

    Update logger.py

--- Registration/utils/logger.py
+++ Registration/utils/logger.py
@@ -9,7 +9,7 @@ class Logger:
         else:
             logger = logging.getLogger(logger_name)
         logger.setLevel(loglevel)
-        fh = logging.FileHandler("C:\\Data\\Python Selenium\\SeleniumCaseStudy\\Registration\\logs\\logfile.log", mode='a')
+        fh = logging.FileHandler("Registration\\logs\\logfile.log", mode='a')
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt= "%d-%m-%Y %I:%M:%S %p")
         fh.setFormatter(formatter)
         logger.addHandler(fh)
